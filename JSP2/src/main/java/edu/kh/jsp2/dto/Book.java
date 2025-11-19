9116ca4201478698a5d90a7330ee07980f549308
khcmh2509 <huni1905@gmail.com>
2025-11-19 17:07:46 +0900

    no message

---
+++ JSP2/src/main/java/edu/kh/jsp2/dto/Book.java
@@ -0,0 +1,31 @@
+package edu.kh.jsp2.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+//@Getter
+//@Setter
+//@ToString
+@Data // Getter + Setter + toString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+	//필드
+	private String title;// 제목
+	private String writer; // 저자
+	private int price; // 가격
+	
+	// 메서드(생성자, getter/setter/toString)
+
+	
+	
+	
+	
+}
